6a5fdd959b2030a011d49e709e1396c8c7e5153d
karimovgit <dev.karimov@gmail.com>
2020-03-31 01:13:49 +0500

    added function to add new item to list

--- src/components/ItemAddForm/index.js
+++ src/components/ItemAddForm/index.js
@@ -1,17 +1,47 @@
 import React from 'react'
 import './ItemAddForm.css'
 
-const ItemAddForm = ({ onAddItem }) => {
-  return (
-    <div className="item-add-form">
-      <button
-        className="btn btn-outline-secondary"
-        onClick={() => onAddItem('lol')}
-      >
-        Add Item
-      </button>
-    </div>
-  )
-}
+export default class ItemAddForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      label: ''
+    }
+  }
 
-export default ItemAddForm
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.onAddItem(this.state.label)
+    this.setState({
+      label: ''
+    })
+  }
+
+  render() {
+    return (
+      <form
+        className="item-add-form d-flex"
+        onSubmit={this.onSubmit}
+      >
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+          value={this.state.label}
+        />
+        <button
+          className="btn btn-outline-secondary"
+        >
+          Add Item
+        </button>
+      </form>
+    )
+  }
+}
